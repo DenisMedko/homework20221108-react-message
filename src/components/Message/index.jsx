d3595bcb233ddc9d8a5863b18ce5830c331371e0
DenisMedko <medko.denis76@gmail.com>
2022-11-09 15:25:47 +0200

    add state to MessageDashboard

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 
 class Message extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isRead: false};
-    }
-    changeStatus = () => {
-        this.setState({isRead: !this.state.isRead});
-    };
+    
     render () {
-        const {sender, text} = this.props;
+        const { id, sender, text, isRead, onClick} = this.props;
+        //debugger;
         return (
             <>
                 <div>Message from : {sender}</div>
                 <div>Text : {text}</div>
-                <button onClick={this.changeStatus} disabled={this.state.isRead}>OK</button>
+                <button id={id} onClick={onClick} disabled={isRead}>OK</button>
             </>
         );
     }
--- src/components/MessageDashboard/index.jsx
+++ src/components/MessageDashboard/index.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import Message from "../Message";
 
 class MessageDashboard extends React.Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {messages: this.props.messages};
+    }
+    changeStatus = (e) => {
+        const {messages : oldStateMessages} = this.state;
+        const newStateMessages = oldStateMessages.map(message => {
+            const isRead = message.id === +e.target.id ? !message.isRead : message.isRead;
+            return {...message, isRead: isRead}
+        });
+        this.setState({messages: newStateMessages});    
+    };
     render () {
-        const {messages} = this.props;
+        //debugger;
+        const {messages} = this.state;
         const messagesList = messages.map(message => <Message 
+            id = {message.id}
             key = {message.id}
             sender = {message.sender}
             text = {message.text}
+            onClick = {this.changeStatus}
+            isRead = {message.isRead}
             />);
         return (
             <>
